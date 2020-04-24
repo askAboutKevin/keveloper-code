1a43e23d8dae54b614dd7e0146f8949511d1e193
keveloper <kdowdy1021@gmail.com>
2020-04-23 22:27:58 -0400

    first commit for website

---
+++ build/precache-manifest.57ad743d4bfed6820aa20367c7c17924.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5daaf0a86389a4de65a6bd2e12139225",
+    "url": "/index.html"
+  },
+  {
+    "revision": "071b26ac8c81a44f6508",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0232d899717c9b8e25f7",
+    "url": "/static/js/2.021a4f51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.021a4f51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "071b26ac8c81a44f6508",
+    "url": "/static/js/main.e6b161ed.chunk.js"
+  },
+  {
+    "revision": "0781f34c4d186d5b7397",
+    "url": "/static/js/runtime-main.3e0ba87e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
